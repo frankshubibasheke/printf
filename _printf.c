9888ff9262f381aba07508382769c722b3b5544f
Frank Shubi Basheke <75610367+frankshubibasheke@users.noreply.github.com>
2022-10-21 16:21:03 -0700

    Task 0

--- _printf.c
+++ _printf.c
@@ -4,9 +4,8 @@ void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - Printf function
- * @format: format
- *
- * Return: Printed chars
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
@@ -39,29 +38,29 @@ int _printf(const char *format, ...)
 			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
-			flags, width, precision, size);
+				flags, width, precision, size);
 			if (printed == -1)
-			return (-1);
+				return (-1);
 			printed_chars += printed;
-			}
-			}
-			
-			print_buffer(buffer, &buff_ind);
-			
-			va_end(list);
-			
-			return (printed_chars);
-			}
-			
-			/**
-			* print_buffer - Prints the contents of the buffer if it exist
-			* @buffer: Array of chars
-			* @buff_ind: Index at which to add next char, represents the length.
-			*/
-			void print_buffer(char buffer[], int *buff_ind)
-			{
-			if (*buff_ind > 0)
-			write(1, &buffer[0], *buff_ind);
-			
-			*buff_ind = 0;
-			}
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
+}
