0cdf158f8ebf922a2101fde3be9a33dc98239a93
Frank Shubi Basheke <75610367+frankshubibasheke@users.noreply.github.com>
2022-10-21 16:29:51 -0700

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -3,7 +3,7 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
+ * printf - Printf function
  * @format: format.
  * Return: Printed chars.
  */
@@ -41,6 +41,7 @@ int _printf(const char *format, ...)
 					flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
+
 			printed_chars += printed;
 		}
 	}
